b1fdc89c7b41e168054c4fd1153cf9bfe72c6963
Kyle Banerjee <kyle.banerjee@gmail.com>
2020-01-11 15:40:19 -0800

    switch node edit to delete and insert

--- itemsfix.sh
+++ itemsfix.sh
@@ -7,6 +7,7 @@ do
 	holding_id="$(cut -d',' -f2 <<<$item)"
 	item_pid="$(cut -d',' -f3 <<<$item)"
 	call_num="$(cut -d',' -f4 <<<$item)"
+	#make sure we only match and delte the correct nodes
 	match_expression="/item/item_data[public_note='Other call numbers: $call_num']"
 	
 	
@@ -16,19 +17,15 @@ do
 	
 	# Extract public note, use character position to remove undesirable parts
 	public_note=$(echo $xmldoc | xmlstarlet sel -T -t -m "$match_expression" -v public_note)
-echo $public_note
-echo $xmldoc |xmlstarlet fo
-	xmldoc=$(echo $xmldoc | xmlstarlet ed -d "$match_expression")
-exit
-	public_note=$(echo $public_note | cut -c21-)
 
-	# update the temp call number and set the public note to nothing
-	xmldoc=$(echo $xmldoc | xmlstarlet edit --update "/item/item_data/temp_call_number" --value "$call_num")
-	xmldoc=$(echo $xmldoc | xmlstarlet edit --update "/item/item_data/public_note" --value "")
+	# delete and insert rather than updating to prevent duplicate fields
+	xmldoc=$(echo $xmldoc | xmlstarlet ed -d "$match_expression")
+
+	xmldoc=$(echo $xmldoc | xmlstarlet ed -d "/item/holding_data/temp_call_number")
+	xmldoc=$(echo $xmldoc | xmlstarlet ed -s "/item/holding_data" -t elem -n "temp_call_number" -v "$call_num")
 	
-	# build the holding record
-	suppress_from_publishing=$(echo $xmldoc | xmlstarlet sel -T -t -m '/item/item_data/public_note' -v '.')
-	#echo $xmldoc |xmlstarlet fo
+	echo $xmldoc |xmlstarlet fo
+exit
 
 	# put the record back
 	results=$(curl -s -H "Authorization: apikey $(cat apikey.txt)" -H "Accept: application/xml" -d "$xmldoc" -X PUT "https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs/${mms_id}/holdings/${holding_id}/items/${item_pid}")
