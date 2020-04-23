6ba2da03eacef43ad34b59dd1b5ef67adf6a49fd
Kyle Banerjee <kyle.banerjee@gmail.com>
2020-04-22 17:55:39 -0700

    add $8 to 866 if there is none present and only one 866

--- holdingsfix.sh
+++ holdingsfix.sh
@@ -14,6 +14,13 @@ do
 	#edit only 852s that don't contain $h
 	xmldoc=$(echo $xmldoc | xmlstarlet ed -u '/holding/record/datafield[@tag="852"][not(subfield[@code="h"])]/@ind1' -v '5')
 
+	#replace bland indicators with "unknown" values
+	xmldoc=$(echo $xmldoc | xmlstarlet ed -u '/holding/record/datafield[@tag="853"][@ind1=" "]/@ind1' -v '3')
+	xmldoc=$(echo $xmldoc | xmlstarlet ed -u '/holding/record/datafield[@tag="853"][@ind2=" "]/@ind2' -v '3')
+
+	#set 866 $8 to zero only if there is one 866 and no $8 is already present
+	xmldoc=$(echo $xmldoc | xmlstarlet ed -s '/holding/record[count(datafield[@tag="866"])="1"]/datafield[@tag="866"][not(subfield[@code="8"])]' -t elem -n 'subfield' -v '0' -i '/holding/record/datafield[@tag="866"]/subfield[not(@code)]' -t attr -n 'code' -v '8' )
+
 	# fix 008
 	# a=yes, b=no, u=unknown
 	#lending="a"
@@ -27,7 +34,7 @@ do
 	# fix material type and in_temp location if broken
 	# check to make sure it worked
 	updatedoc=$(curl -s -H "Authorization: apikey $(cat apikey.txt)" -H "Content-Type: application/xml" -X PUT --data "${xmldoc}" $getstring)
-	ind1=$(echo $updatedoc |xmlstarlet sel -t -m '/holding/record/datafield[@tag="852"][not(subfield[@code="h"])]/@ind1')
+	ind1=$(echo $updatedoc |xmlstarlet sel -t -m '/holding/record/datafield[@tag="852"][not(subfield[@code="h"])]/@ind1' -v '.')
 	echo 'Ind1 has been assigned "'$ind1'" to '$mms_id",$holding_id"
 	echo "$mms_id,$holding_id" >> completed
 done
